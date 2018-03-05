ae263438ff7898214c667f7958249c94943d3ebe
Lucas Moura <lucas.moura128@gmail.com>
2018-03-04 22:08:57 -0300

    Tokenize songs in dataset

--- preprocessing/dataset.py
+++ preprocessing/dataset.py
@@ -1,12 +1,9 @@
 import os
 import random
 import pickle
+import re
 
 from pathlib import Path
-from collections import namedtuple
-
-
-Song = namedtuple('Song', ['song_text', 'num_words'])
 
 
 class MusicDataset:
@@ -21,6 +18,20 @@ class MusicDataset:
 
     def get_num_words_from_song(self, song):
         return len(song.replace('\n', ' ').split(' '))
+
+    def format_song_text(self, song_text):
+        song_text = re.sub(r",", " , ", song_text)
+        song_text = re.sub(r"!", " ! ", song_text)
+        song_text = re.sub(r"\?", " ? ", song_text)
+
+        song_text = '<begin> ' + song_text + ' <end>'
+        song_text = re.sub(r"\n", ' <br> ', song_text)
+        song_text = re.sub(r'\s{2,}', ' ', song_text)
+        song_text = re.sub('<br> <br>', '<br> <par>', song_text)
+
+        song_text = song_text.split(' ')
+
+        return song_text
 
     def get_songs(self):
         self.all_songs = []
@@ -37,23 +48,21 @@ class MusicDataset:
 
                 song_path = artist_path / song
                 song_text = self.read_song(song_path)
-                num_words = self.get_num_words_from_song(song_text)
 
-                song = Song(song_text, num_words)
-
-                self.all_songs.append(song)
+                song_text = self.format_song_text(song_text)
+                self.all_songs.append(song_text)
 
     def average_song_size(self):
         avg_size = 0
         for song in self.all_songs:
-            avg_size += song.num_words
+            avg_size += len(song)
 
         return avg_size / len(self.all_songs)
 
     def load_dataset(self, dataset_type):
         dataset_path = self.dataset_save_path / dataset_type
 
-        with dataset_path.open('rb') as dataset_file:
+        with dataset_path.open(mode='rb') as dataset_file:
             return pickle.load(dataset_file)
 
     def load_datasets(self):
@@ -61,9 +70,35 @@ class MusicDataset:
             return False
 
         print('Loading datasets ...')
-        self.train_dataset = self.load_dataset('train')
-        self.validation_dataset = self.load_dataset('validation')
-        self.test_dataset = self.load_dataset('test')
+        self.all_songs = self.load_dataset('all_songs.pkl')
+        self.train_dataset = self.load_dataset('train.pkl')
+        self.validation_dataset = self.load_dataset('validation.pkl')
+        self.test_dataset = self.load_dataset('test.pkl')
+
+        return True
+
+    def save_dataset(self, dataset, dataset_type):
+        dataset_path = self.dataset_save_path / dataset_type
+
+        with dataset_path.open(mode='wb') as dataset_file:
+            pickle.dump(dataset, dataset_file)
+
+    def save_datasets(self):
+
+        if not self.dataset_save_path.is_dir():
+            self.dataset_save_path.mkdir()
+
+        dataset_type = 'all_songs.pkl'
+        self.save_dataset(self.all_songs, dataset_type)
+
+        dataset_type = 'train.pkl'
+        self.save_dataset(self.train_dataset, dataset_type)
+
+        dataset_type = 'validation.pkl'
+        self.save_dataset(self.validation_dataset, dataset_type)
+
+        dataset_type = 'test.pkl'
+        self.save_dataset(self.test_dataset, dataset_type)
 
     def split_dataset(self, validation_percent, test_percent):
         total_size = validation_percent + test_percent
@@ -82,8 +117,12 @@ class MusicDataset:
             print('Creating dataset ...')
             self.get_songs()
             self.split_dataset(validation_percent, test_percent)
-            self.save_dataset()
+            self.save_datasets()
 
     def display_info(self):
         print('Total number of songs: {}'.format(len(self.all_songs)))
         print('Average size of songs: {} words'.format(int(self.average_song_size())))
+
+        print('Train size: {}'.format(len(self.train_dataset)))
+        print('Validation size: {}'.format(len(self.validation_dataset)))
+        print('Test size: {}'.format(len(self.test_dataset)))
